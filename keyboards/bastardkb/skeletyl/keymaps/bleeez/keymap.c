193d9aabf2ca0b7c17eb1f36dea8e71e0b2ff790
Maximilian Bauer <bauer.maximilian@web.de>
2022-05-21 10:27:12 +0200

    layout: add more vim navigation

--- keyboards/bastardkb/skeletyl/keymaps/bleeez/keymap.c
+++ keyboards/bastardkb/skeletyl/keymaps/bleeez/keymap.c
@@ -206,79 +206,79 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                 SFT_T(KC_ENT),NAV_SPC,NUM_TAB,   LOW_DEL,RAI_ESC,SFT_T(KC_BSPC)
     ),
 /*
-               ,       , Find  , Build ,       ,                               ,   0   ,^(dead),   $   ,       ,
+               ,       , Find  , Build ,       ,                            0  ,^(dead),   '   ,`(dead),   $   ,
          Undo  ,  Cut  , Copy  , Paste ,       ,                        KC_LEFT,KC_DOWN, KC_UP ,KC_RGHT,       ,
                ,       ,       ,  All  ,       ,                               ,  HOME ,word <-,word ->,  END  ,
 */
     //MAC
     [_NAV_MAC] = LAYOUT_split_3x5_3(
-        _______,_______,C(KC_F),C(KC_B),_______,                        _______, KC_0  ,DM_CIRC,DM_DLR ,_______,
+        _______,_______,C(KC_F),C(KC_B),_______,                         KC_0  ,DM_CIRC,DM_QUOT,DM_GRV ,DM_DLR ,
         C(KC_Y),C(KC_X),C(KC_C),C(KC_V),_______,                        KC_LEFT,KC_DOWN, KC_UP ,KC_RGHT,_______,
         _______,_______,_______,C(KC_A),_______,                        _______,KC_HOME,DM_WGL ,DM_WGR , KC_END,
                                 _______,_______,_______,        _______,_______,_______
     ),
     //Windows
     [_NAV_WIN] = LAYOUT_split_3x5_3(
-        _______,_______,C(KC_F),C(KC_B),_______,                        _______, KC_0  ,DW_CIRC,DW_DLR ,_______,
+        _______,_______,C(KC_F),C(KC_B),_______,                         KC_0  ,DW_CIRC,DW_QUOT,DW_GRV ,DW_DLR ,
         C(KC_Y),C(KC_X),C(KC_C),C(KC_V),_______,                        KC_LEFT,KC_DOWN, KC_UP ,KC_RGHT,_______,
         _______,_______,_______,C(KC_A),_______,                        _______,KC_HOME,DW_WGL ,DW_WGR , KC_END,
                                 _______,_______,_______,        _______,_______,_______
     ),
 /*
-               ,       ,       ,       ,       ,                               ,   7   ,   8   ,   9   ,   *   ,
-               ,OS_CHANGE,Bright+, VOL +, MUTE ,                           /   ,   4   ,   5   ,   6   ,   0   ,
+               ,       ,       ,       ,       ,                               ,   7   ,   8   ,   9   ,       ,
+               ,OS_CHANGE,Bright+, VOL +, MUTE ,                           *   ,   4   ,   5   ,   6   ,   0   ,
                ,       ,Bright-, VOL - ,       ,                               ,   1   ,   2   ,   3   ,   +   ,
 */
     //MAC
     [_NUMBER_MAC] = LAYOUT_split_3x5_3(
-        _______,_______,_______,_______,_______,                        _______, KC_7  , KC_8  , KC_9  ,DM_ASTR,
-        _______,OS_CHANGE,KC_BRIU,KC_VOLU,KC_MUTE,                      DM_SLSH, KC_4  , KC_5  , KC_6  , KC_0  ,
+        _______,_______,_______,_______,_______,                        _______, KC_7  , KC_8  , KC_9  ,_______,
+        _______,OS_CHANGE,KC_BRIU,KC_VOLU,KC_MUTE,                      DM_ASTR, KC_4  , KC_5  , KC_6  , KC_0  ,
         _______,_______,KC_BRID,KC_VOLD,_______,                        _______, KC_1  , KC_2  , KC_3  ,DM_PLUS,
                                 _______,_______,_______,        _______,_______,_______
     ),
     //Windows
     [_NUMBER_WIN] = LAYOUT_split_3x5_3(
-        _______,_______,_______,_______,_______,                        _______, KC_7  , KC_8  , KC_9  ,DW_ASTR,
-        _______,OS_CHANGE,KC_BRIU,KC_VOLU,KC_MUTE,                      DW_SLSH, KC_4  , KC_5  , KC_6  , KC_0  ,
+        _______,_______,_______,_______,_______,                        _______, KC_7  , KC_8  , KC_9  ,_______,
+        _______,OS_CHANGE,KC_BRIU,KC_VOLU,KC_MUTE,                      DW_ASTR, KC_4  , KC_5  , KC_6  , KC_0  ,
         _______,_______,KC_BRID,KC_VOLD,_______,                        _______, KC_1  , KC_2  , KC_3  ,DW_PLUS,
                                 _______,_______,_______,        _______,_______,_______
     ),
 /*
-        `(dead),   <   ,   >   ,   [   ,   #   ,                                ,   7   ,   8   ,   9   ,       ,
+           [   ,   <   ,   >   ,   !   ,   #   ,                                ,   7   ,   8   ,   9   ,       ,
            "   ,   (   ,   )   ,   =   ,   /   ,                            *   ,   4   ,   5   ,   6   ,   0   ,
-           '   ,   {   ,   }   ,   ]   ,   \   ,                         ´(dead),   1   ,   2   ,   3   ,   +   ,
+           ]   ,   {   ,   }   ,   ?   ,   \   ,                         ´(dead),   1   ,   2   ,   3   ,   +   ,
 */
     //MAC
     [_RAISE_MAC] = LAYOUT_split_3x5_3(
-         DM_GRV,DM_LABK,DM_RABK,DM_LBRC,DM_HASH,                          _______, KC_7  , KC_8  , KC_9  ,_______,
+        DM_LBRC,DM_LABK,DM_RABK,DM_EXLM,DM_HASH,                          _______, KC_7  , KC_8  , KC_9  ,_______,
         DM_DQUO,DM_LPRN,DM_RPRN,DM_EQL ,DM_SLSH,                          DM_ASTR, KC_4  , KC_5  , KC_6  , KC_0  ,
-        DM_QUOT,DM_LCBR,DM_RCBR,DM_RBRC,DM_BSLS,                          DM_ACUT, KC_1  , KC_2  , KC_3  ,DM_PLUS,
+        DM_RBRC,DM_LCBR,DM_RCBR,DM_QUES,DM_BSLS,                          DM_ACUT, KC_1  , KC_2  , KC_3  ,DM_PLUS,
                                 _______,_______,_______,        _______,_______,_______
     ),
     //Windows
     [_RAISE_WIN] = LAYOUT_split_3x5_3(
-         DW_GRV,DW_LABK,DW_RABK,DW_LBRC,DW_HASH,                          _______, KC_7  , KC_8  , KC_9  ,_______,
+        DW_LBRC,DW_LABK,DW_RABK,DW_EXLM,DW_HASH,                          _______, KC_7  , KC_8  , KC_9  ,_______,
         DW_DQUO,DW_LPRN,DW_RPRN,DW_EQL ,DW_SLSH,                          DW_ASTR, KC_4  , KC_5  , KC_6  , KC_0  ,
-        DW_QUOT,DW_LCBR,DW_RCBR,DW_RBRC,DW_BSLS,                          DW_ACUT, KC_1  , KC_2  , KC_3  ,DW_PLUS,
+        DW_RBRC,DW_LCBR,DW_RCBR,DW_QUES,DW_BSLS,                          DW_ACUT, KC_1  , KC_2  , KC_3  ,DW_PLUS,
                                 _______,_______,_______,        _______,_______,_______
     ),
 /*
            °   ,   µ   ,   €   ,   @   ,   ß   ,                                ,  F7   ,  F8   ,  F9   ,  F12  ,
-           |   ,   &   ,   !   ,   ?   , PRINT ,                                ,  F4   ,  F5   ,  F6   ,  F11  ,
-           %   ,   ü   ,   ö   ,   ä   ,       ,                                ,  F1   ,  F2   ,  F3   ,  F10  ,
+           |   ,   &   ,   ü   ,   ö   ,   ä   ,                                ,  F4   ,  F5   ,  F6   ,  F11  ,
+           %   ,       ,       ,~(dead), PRINT ,                                ,  F1   ,  F2   ,  F3   ,  F10  ,
 */
     //MAC
     [_LOWER_MAC] = LAYOUT_split_3x5_3(
         DM_DEG ,DM_MICR,DM_EURO, DM_AT , DM_SS ,                        _______, KC_F7 , KC_F8 , KC_F9 ,KC_F12 ,
-        DM_PIPE,DM_AMPR,DM_EXLM,DM_QUES,KC_PSCR,                        _______, KC_F4 , KC_F5 , KC_F6 ,KC_F11 ,
-        DM_PERC,DM_UDIA,DM_ODIA,DM_ADIA,_______,                        _______, KC_F1 , KC_F2 , KC_F3 ,KC_F10 ,
+        DM_PIPE,DM_AMPR,DM_UDIA,DM_ODIA,DM_ADIA,                        _______, KC_F4 , KC_F5 , KC_F6 ,KC_F11 ,
+        DM_PERC,_______,_______,DM_TILD,KC_PSCR,                        _______, KC_F1 , KC_F2 , KC_F3 ,KC_F10 ,
                                 _______,_______,_______,        _______,_______,_______
     ),
     //Windows
     [_LOWER_WIN] = LAYOUT_split_3x5_3(
         DW_DEG ,DW_MICR,DW_EURO, DW_AT , DW_SS ,                        _______, KC_F7 , KC_F8 , KC_F9 ,KC_F12 ,
-        DW_PIPE,DW_AMPR,DW_EXLM,DW_QUES,KC_PSCR,                        _______, KC_F4 , KC_F5 , KC_F6 ,KC_F11 ,
-        DW_PERC,DW_UDIA,DW_ODIA,DW_ADIA,_______,                        _______, KC_F1 , KC_F2 , KC_F3 ,KC_F10 ,
+        DW_PIPE,DW_AMPR,DW_UDIA,DW_ODIA,DW_ADIA,                        _______, KC_F4 , KC_F5 , KC_F6 ,KC_F11 ,
+        DW_PERC,_______,_______,DW_TILD,KC_PSCR,                        _______, KC_F1 , KC_F2 , KC_F3 ,KC_F10 ,
                                 _______,_______,_______,        _______,_______,_______
     ),
 
--- keyboards/handwired/dactyl_manuform/5x6/keymaps/bleeez/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/bleeez/keymap.c
@@ -214,14 +214,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                         KC_LALT,KC_CALC,/*CALC=RPI-KVM-Switch*/  OS_CHANGE, ADJUST
     ),
 /*
-               ,       ,       , Find  , Build ,       ,                               ,   0   ,^(dead),   $   ,       ,       ,
+               ,       ,       , Find  , Build ,       ,                            0  ,^(dead),   '   ,`(dead),   $   ,       ,
                , Undo  ,  Cut  , Copy  , Paste ,       ,                        KC_LEFT,KC_DOWN, KC_UP ,KC_RGHT,       ,       ,
                ,       ,       ,       ,  All  ,       ,                               ,  HOME ,word <-,word ->,  END  ,       ,
 */
     //MAC
     [_NAV_MAC] = LAYOUT_5x6(
         _______,_______,_______,_______,_______,_______,                        _______,_______,_______,_______,_______,_______,
-        _______,_______,_______,C(KC_F),C(KC_B),_______,                        _______, KC_0  ,DM_CIRC,DM_DLR ,_______,_______,
+        _______,_______,_______,C(KC_F),C(KC_B),_______,                         KC_0  ,DM_CIRC,DM_QUOT,DM_GRV ,DM_DLR ,_______,
         _______,C(KC_Y),C(KC_X),C(KC_C),C(KC_V),_______,                        KC_LEFT,KC_DOWN, KC_UP ,KC_RGHT,_______,_______,
         _______,_______,_______,_______,C(KC_A),_______,                        _______,KC_HOME,DM_WGL ,DM_WGR ,KC_END,_______,
                         _______,_______,                                                        _______,_______,
@@ -232,7 +232,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     //Windows
     [_NAV_WIN] = LAYOUT_5x6(
         _______,_______,_______,_______,_______,_______,                        _______,_______,_______,_______,_______,_______,
-        _______,_______,_______,C(KC_F),C(KC_B),_______,                        _______, KC_0  ,DW_CIRC,DW_DLR ,_______,_______,
+        _______,_______,_______,C(KC_F),C(KC_B),_______,                         KC_0  ,DW_CIRC,DW_QUOT,DW_GRV ,DM_DLR ,_______,
         _______,C(KC_Y),C(KC_X),C(KC_C),C(KC_V),_______,                        KC_LEFT,KC_DOWN, KC_UP ,KC_RGHT,_______,_______,
         _______,_______,_______,_______,C(KC_A),_______,                        _______,KC_HOME,DW_WGL ,DW_WGR ,KC_END,_______,
                         _______,_______,                                                        _______,_______,
@@ -241,15 +241,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                         _______,_______,                        _______,_______
     ),
 /*
-               ,       ,       ,       ,       ,       ,                               ,   7   ,   8   ,   9   ,   *   ,       ,
-               ,       ,       ,Bright+, VOL + , MUTE  ,                           /   ,   4   ,   5   ,   6   ,   0   ,       ,
+               ,       ,       ,       ,       ,       ,                               ,   7   ,   8   ,   9   ,       ,       ,
+               ,       ,       ,Bright+, VOL + , MUTE  ,                           *   ,   4   ,   5   ,   6   ,   0   ,       ,
                ,       ,       ,Bright-, VOL - ,       ,                               ,   1   ,   2   ,   3   ,   +   ,       ,
 */
     //MAC
     [_NUMBER_MAC] = LAYOUT_5x6(
         _______,_______,_______,_______,_______,_______,                        _______,_______,_______,_______,_______,_______,
-        _______,_______,_______,_______,_______,_______,                        _______, KC_7  , KC_8  , KC_9  ,DM_ASTR,_______,
-        _______,_______,_______,KC_BRIU,KC_VOLU,KC_MUTE,                        DM_SLSH, KC_4  , KC_5  , KC_6  , KC_0  ,_______,
+        _______,_______,_______,_______,_______,_______,                        _______, KC_7  , KC_8  , KC_9  ,_______,_______,
+        _______,_______,_______,KC_BRIU,KC_VOLU,KC_MUTE,                        DM_ASTR, KC_4  , KC_5  , KC_6  , KC_0  ,_______,
         _______,_______,_______,KC_BRID,KC_VOLD,_______,                        _______, KC_1  , KC_2  , KC_3  ,DM_PLUS,_______,
                         _______,_______,                                                        _______,_______,
                                         _______,_______,                        _______,_______,
@@ -259,8 +259,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     //Windows
     [_NUMBER_WIN] = LAYOUT_5x6(
         _______,_______,_______,_______,_______,_______,                        _______,_______,_______,_______,_______,_______,
-        _______,_______,_______,_______,_______,_______,                        _______, KC_7  , KC_8  , KC_9  ,DW_ASTR,_______,
-        _______,_______,_______,KC_BRIU,KC_VOLU,KC_MUTE,                        DW_SLSH, KC_4  , KC_5  , KC_6  , KC_0  ,_______,
+        _______,_______,_______,_______,_______,_______,                        _______, KC_7  , KC_8  , KC_9  ,_______,_______,
+        _______,_______,_______,KC_BRIU,KC_VOLU,KC_MUTE,                        DM_ASTR, KC_4  , KC_5  , KC_6  , KC_0  ,_______,
         _______,_______,_______,KC_BRID,KC_VOLD,_______,                        _______, KC_1  , KC_2  , KC_3  ,DW_PLUS,_______,
                         _______,_______,                                                        _______,_______,
                                         _______,_______,                        _______,_______,
@@ -268,16 +268,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                         _______,_______,                        _______,_______
     ),
 /*
-               ,`(dead),   <   ,   >   ,   [   ,   #   ,                                ,   7   ,   8   ,   9   ,       ,       ,
-               ,   "   ,   (   ,   )   ,   =   ,   /   ,                            *   ,   4   ,   5   ,   6   ,   0   ,       ,
-               ,   '   ,   {   ,   }   ,   ]   ,   \   ,                         ´(dead),   1   ,   2   ,   3   ,   +   ,       ,
+              ,    [   ,   <   ,   >   ,   !   ,   #   ,                                ,   7   ,   8   ,   9   ,       ,
+              ,    "   ,   (   ,   )   ,   =   ,   /   ,                            *   ,   4   ,   5   ,   6   ,   0   ,
+              ,    ]   ,   {   ,   }   ,   ?   ,   \   ,                         ´(dead),   1   ,   2   ,   3   ,   +   ,
 */
     //MAC
     [_RAISE_MAC] = LAYOUT_5x6(
         _______,_______,_______,_______,_______,_______,                          _______,_______,_______,_______,_______,_______,
-        _______, DM_GRV,DM_LABK,DM_RABK,DM_LBRC,DM_HASH,                          _______, KC_7  , KC_8  , KC_9  ,_______,_______,
+        _______,DM_LBRC,DM_LABK,DM_RABK,DM_EXLM,DM_HASH,                          _______, KC_7  , KC_8  , KC_9  ,_______,_______,
         _______,DM_DQUO,DM_LPRN,DM_RPRN,DM_EQL ,DM_SLSH,                          DM_ASTR, KC_4  , KC_5  , KC_6  , KC_0  ,_______,
-        _______,DM_QUOT,DM_LCBR,DM_RCBR,DM_RBRC,DM_BSLS,                          DM_ACUT, KC_1  , KC_2  , KC_3  ,DM_PLUS,_______,
+        _______,DM_RBRC,DM_LCBR,DM_RCBR,DM_QUES,DM_BSLS,                          DM_ACUT, KC_1  , KC_2  , KC_3  ,DM_PLUS,_______,
                         _______,_______,                                                          _______,_______,
                                         _______,_______,                          _______,_______,
                                         _______,_______,                          _______,_______,
@@ -286,9 +286,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     //Windows
     [_RAISE_WIN] = LAYOUT_5x6(
         _______,_______,_______,_______,_______,_______,                          _______,_______,_______,_______,_______,_______,
-        _______, DW_GRV,DW_LABK,DW_RABK,DW_LBRC,DW_HASH,                          _______, KC_7  , KC_8  , KC_9  ,_______,_______,
+        _______,DW_LBRC,DW_LABK,DW_RABK,DW_EXLM,DW_HASH,                          _______, KC_7  , KC_8  , KC_9  ,_______,_______,
         _______,DW_DQUO,DW_LPRN,DW_RPRN,DW_EQL ,DW_SLSH,                          DW_ASTR, KC_4  , KC_5  , KC_6  , KC_0  ,_______,
-        _______,DW_QUOT,DW_LCBR,DW_RCBR,DW_RBRC,DW_BSLS,                          DW_ACUT, KC_1  , KC_2  , KC_3  ,DW_PLUS,_______,
+        _______,DW_RCBR,DW_LCBR,DW_RBRC,DW_QUES,DW_BSLS,                          DW_ACUT, KC_1  , KC_2  , KC_3  ,DW_PLUS,_______,
                         _______,_______,                                                          _______,_______,
                                         _______,_______,                          _______,_______,
                                         _______,_______,                          _______,_______,
@@ -296,15 +296,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 /*
                ,   °   ,   µ   ,   €   ,   @   ,   ß   ,                                ,  F7   ,  F8   ,  F9   ,  F12  ,       ,
-               ,   |   ,   &   ,   !   ,   ?   , PRINT ,                                ,  F4   ,  F5   ,  F6   ,  F11  ,       ,
-               ,   %   ,   ü   ,   ö   ,   ä   ,       ,                                ,  F1   ,  F2   ,  F3   ,  F10  ,       ,
+               ,   |   ,   &   ,   ü   ,   ö   ,   ä   ,                                ,  F4   ,  F5   ,  F6   ,  F11  ,       ,
+               ,   %   ,       ,       ,~(dead), PRINT ,                                ,  F1   ,  F2   ,  F3   ,  F10  ,       ,
 */
     //MAC
     [_LOWER_MAC] = LAYOUT_5x6(
         _______,_______,_______,_______,_______,_______,                        _______,_______,_______,_______,_______,_______,
         _______,DM_DEG ,DM_MICR,DM_EURO, DM_AT , DM_SS ,                        _______, KC_F7 , KC_F8 , KC_F9 ,KC_F12 ,_______,
-        _______,DM_PIPE,DM_AMPR,DM_EXLM,DM_QUES,KC_PSCR,                        _______, KC_F4 , KC_F5 , KC_F6 ,KC_F11 ,_______,
-        _______,DM_PERC,DM_UDIA,DM_ODIA,DM_ADIA,_______,                        _______, KC_F1 , KC_F2 , KC_F3 ,KC_F10 ,_______,
+        _______,DM_PIPE,DM_AMPR,DM_UDIA,DM_ODIA,DM_ADIA,                        _______, KC_F4 , KC_F5 , KC_F6 ,KC_F11 ,_______,
+        _______,DM_PERC,_______,_______,DM_TILD,KC_PSCR,                        _______, KC_F1 , KC_F2 , KC_F3 ,KC_F10 ,_______,
                         _______,_______,                                                        _______,_______,
                                         _______,_______,                        _______,_______,
                                         _______,_______,                        _______,_______,
@@ -314,8 +314,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_LOWER_WIN] = LAYOUT_5x6(
         _______,_______,_______,_______,_______,_______,                        _______,_______,_______,_______,_______,_______,
         _______,DW_DEG ,DW_MICR,DW_EURO, DW_AT , DW_SS ,                        _______, KC_F7 , KC_F8 , KC_F9 ,KC_F12 ,_______,
-        _______,DW_PIPE,DW_AMPR,DW_EXLM,DW_QUES,KC_PSCR,                        _______, KC_F4 , KC_F5 , KC_F6 ,KC_F11 ,_______,
-        _______,DW_PERC,DW_UDIA,DW_ODIA,DW_ADIA,_______,                        _______, KC_F1 , KC_F2 , KC_F3 ,KC_F10 ,_______,
+        _______,DW_PIPE,DW_AMPR,DW_UDIA,DW_ODIA,DW_ADIA,                        _______, KC_F4 , KC_F5 , KC_F6 ,KC_F11 ,_______,
+        _______,DW_PERC,_______,_______,DW_TILD,KC_PSCR,                        _______, KC_F1 , KC_F2 , KC_F3 ,KC_F10 ,_______,
                         _______,_______,                                                        _______,_______,
                                         _______,_______,                        _______,_______,
                                         _______,_______,                        _______,_______,
